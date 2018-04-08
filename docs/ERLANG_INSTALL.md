ded8723c94b4be246b3c105f163d99b2d2adf2f5
Juraj Hlista <jurajhlista@gmail.com>
2018-04-08 21:35:02 +0200

    Use asdf to manage erlang, elixir and rebar installations

---
+++ docs/ERLANG_INSTALL.md
@@ -0,0 +1,14 @@
+# Erlang installation
+
+Use `asdf` to manage `erlang` installations. `asdf` uses `kerl` under the hood.
+To modify default `erlang` installation setting, source `kerlrc` file:
+
+```bash
+source dotfiles/kerl/kerlrc
+```
+
+Install `erlang` with `asdf`:
+
+```bash
+asdf install erlang 20.3
+```
--- mac_init/dev.sh
+++ mac_init/dev.sh
@@ -5,3 +5,13 @@ curl https://sh.rustup.rs -sSf | sh
 
 # Install libsodium - required by some projects (not always needed).
 brew install libsodium
+
+# Install packages needed to build erlang with asdf.
+brew install wxmac
+
+# Install asdf and plugins.
+brew install asdf
+# Check ERLANG_INSTALL.md
+asdf plugin-add erlang
+asdf plugin-add rebar
+asdf plugin-add elixir
