d3779379cc77f3e8dfa64250b271c36f5f94f88f
Juraj Hlista <jurajhlista@gmail.com>
2019-04-21 19:02:31 +0200

    Remoce --allhomes option for dockutil

--- mac_init/dock.sh
+++ mac_init/dock.sh
@@ -2,21 +2,22 @@
 
 # Items in mac's dock.
 
-dockutil --remove all --allhomes
+dockutil --remove all
 
-dockutil --add '~/Downloads' --view grid --display folder --allhomes
+dockutil --add '~/Downloads' --view grid --display folder
 
-dockutil --add /Applications/iTerm.app --allhomes
-dockutil --add /Applications/Google\ Chrome.app --allhomes
-dockutil --add /Applications/Brave.app --allhomes
-dockutil --add /Applications/TorBrowser.app --allhomes
-dockutil --add /Applications/Evernote.app --allhomes
-dockutil --add /Applications/MacDown.app --allhomes
-dockutil --add /Applications/MacPass.app --allhomes
-dockutil --add /Applications/Calendar.app --allhome
-dockutil --add /Applications/Signal.app --allhomes
-dockutil --add /Applications/VLC.app --allhomes
-dockutil --add /Applications/Slack.app --allhomes
-dockutil --add /Applications/VirtualBox.app --allhomes
-dockutil --add /Applications/Spotify.app --allhomes
-dockutil --add /Applications/System\ Preferences.app --allhomes
+dockutil --add /Applications/iTerm.app
+dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/Brave.app
+dockutil --add /Applications/TorBrowser.app
+dockutil --add /Applications/Evernote.app
+dockutil --add /Applications/MacDown.app
+dockutil --add /Applications/MacPass.app
+dockutil --add /Applications/Calendar.app
+dockutil --add /Applications/Signal.app
+dockutil --add /Applications/VLC.app
+dockutil --add /Applications/Slack.app
+dockutil --add /Applications/VirtualBox.app
+dockutil --add /Applications/Spotify.app
+dockutil --add /Applications/System\ Preferences.app
+
