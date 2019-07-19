389c499ef1aa723213b3b1182b905801f1afefd5
Juraj Hlista <jurajhlista@gmail.com>
2019-07-19 16:27:34 +0200

    Add iosevka font

--- mac_init/brew_cask.sh
+++ mac_init/brew_cask.sh
@@ -19,6 +19,7 @@ brew cask install font-inconsolata
 brew cask install font-inconsolata-for-powerline
 brew cask install font-source-code-pro-for-powerline
 brew cask install font-hack
+brew cask install font-iosevka-nerd-font-mono
 
 brew tap dteoh/sqa
 brew cask install slowquitapps
